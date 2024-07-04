e2cf3126edcd0e42403f290af7ca05c475af5819
max <m.titov@p-group.ru>
2024-07-04 14:44:09 +0300

    feat: #Add VOIP Push for IOS

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,46 @@ import { formatPrivateKey } from './formatPrivateKey'
 import { pushNotification } from './pushNotification'
 
 export default {
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    if (request.method === 'POST') {
+      const message = await request.json();
+      //@ts-ignore
+      const { event, deviceToken, body, title, subtitle, badge, threadId, voip } = message
+      const privateKey = formatPrivateKey(env.P8)
+      await pushNotification(deviceToken, privateKey, event, body, env, title, subtitle, badge, threadId, voip)
+      // console.log(`Received message: ${message.text}`);
+      return new Response("Message received");
+    }
+    return new Response("Hello, World!");
+  },
   async queue(batch, env): Promise<void> {
     const privateKey = formatPrivateKey(env.P8)
 
     for (const message of batch.messages) {
-      const { event, deviceToken, body, title, subtitle, badge, threadId } = message.body
+      const { 
+        event,
+        deviceToken,
+        body,
+        title,
+        subtitle,
+        badge,
+        threadId,
+        voip,
+      } = message.body
 
       try {
-        await pushNotification(deviceToken, privateKey, event, body, title, subtitle, badge, threadId, env)
+        await pushNotification(
+          deviceToken,
+          privateKey,
+          event,
+          body,
+          env,
+          title,
+          subtitle,
+          badge,
+          threadId,
+          voip
+        )
         message.ack()
       } catch (error) {
         console.error('Failed to push notification:', error)
--- src/sendPushRequest.ts
+++ src/sendPushRequest.ts
@@ -5,16 +5,26 @@ export const sendPushRequest = async (
   jwt: string,
   notification: IOSNotification,
   { APNS_TOPIC }: Env,
+  voip?: boolean,
+  deviceToken?: string
 ): Promise<Response> => {
-  console.log(notification)
-  return fetch(url, {
-    method: 'POST',
-    headers: {
+  const headers = voip ? {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + jwt,
+    'apns-topic': `${APNS_TOPIC}.voip`,
+    'apns-push-type': `voip`,
+    'apns-priority': 10
+  }
+    : {
       'Content-Type': 'application/json',
       Authorization: 'Bearer ' + jwt,
       'apns-topic': APNS_TOPIC,
-    },
-
+    };
+  console.log(notification)
+  return fetch(url, {
+    method: 'POST',
+    //@ts-ignore
+    headers,
     body: JSON.stringify(notification),
   })
 }
